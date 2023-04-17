616417a800a0ad203e504ee77eaf0af2f477ccf7
colorpen2010 <melnik.olga.22.11.1981@gmail.com>
2023-04-17 12:06:42 +0300

    version 2

--- view.py
+++ view.py
@@ -4,10 +4,16 @@ screen=pygame.display.set_mode([800,600])
 def ion():
     kote_kyb=pygame.rect.Rect(400,600-128,169,128)
     kote=pygame.image.load('real kartinki/cat1.png')
+
+    zontik_kyb=pygame.rect.Rect(475,395,102,102)
+    zontik=pygame.image.load('real kartinki/umbrella.png')
+    zontik=pygame.transform.scale(zontik,[100,100])
     if model.show_rects == True:
         kyb=pygame.draw.rect(screen,[0,200,0],kote_kyb,4)
+        zonkyb = pygame.draw.rect(screen, [200,0, 0], zontik_kyb, 4)
 
 
     screen.blit(kote,kote_kyb)
+    screen.blit(zontik,zontik_kyb)
 
     pygame.display.flip()
