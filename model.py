69beccca21c2a2cca84d12e8f7246b6c0ec05ae6
colorpen2010 <melnik.olga.22.11.1981@gmail.com>
2023-05-02 12:46:35 +0300

    version 8a

--- model.py
+++ model.py
@@ -1,21 +1,30 @@
-import pygame,random
-show_rects=False
-x=400
-y=472
-katflip=0
-ontroloblak=0
+import pygame, random
+
+show_rects = False
+x = 400
+y = 472
+katflip = 0
+ontroloblak = 0
+
 
 def oblako_letit():
-    global ontroloblak
+    global ontroloblak, caplya_kyb
 
-    if oblako_kyb.right>=800:
-        ontroloblak=1
-    if oblako_kyb.left<=0:
-        ontroloblak=0
-    if ontroloblak==0:
+    if oblako_kyb.right >= 800:
+        ontroloblak = 1
+    if oblako_kyb.left <= 0:
+        ontroloblak = 0
+    if ontroloblak == 0:
         oblako_kyb.left += 3
-    elif ontroloblak==1:
+    elif ontroloblak == 1:
         oblako_kyb.left -= 3
+    caplya_kyb.y += 3
+    if caplya_kyb.bottom>=600 :
+        caplya_kyb.centery=oblako_kyb.centery
+        caplya_kyb.centerx= oblako_kyb.centerx
+    if caplya_kyb.colliderect(vedro_kyb):
+        caplya_kyb.centery=oblako_kyb.centery
+        caplya_kyb.centerx= oblako_kyb.centerx
 
 def control_left():
     global katflip
@@ -23,6 +32,7 @@ def control_left():
         kote_kyb.x += 130
     move_left()
     katflip = 0
+
 
 def control_right():
     global katflip
@@ -33,30 +43,34 @@ def control_right():
 
 
 def move_subjects():
-    zontik_kyb.top=kote_kyb.top-77
-    zontik_kyb.right=kote_kyb.right+22
+    zontik_kyb.top = kote_kyb.top - 77
+    zontik_kyb.right = kote_kyb.right + 22
 
-    vedro_kyb.top=kote_kyb.top-32
-    vedro_kyb.left=kote_kyb.left-15
+    vedro_kyb.top = kote_kyb.top - 32
+    vedro_kyb.left = kote_kyb.left - 15
+
 
 def move_left():
-    kote_kyb.left-=3
-    if kote_kyb.left<=18:
-        kote_kyb.left=18
+    kote_kyb.left -= 3
+    if kote_kyb.left <= 18:
+        kote_kyb.left = 18
     move_subjects()
 
+
 def move_right():
-    kote_kyb.left+=3
-    if kote_kyb.right>=780:
-        kote_kyb.right=780
+    kote_kyb.left += 3
+    if kote_kyb.right >= 780:
+        kote_kyb.right = 780
     vedro_kyb.top = kote_kyb.top - 32
     zontik_kyb.top = kote_kyb.top - 77
     zontik_kyb.right = kote_kyb.left + 80
-    vedro_kyb.left=kote_kyb.right-50
+    vedro_kyb.left = kote_kyb.right - 50
 
 
-oblako_kyb=pygame.rect.Rect(50,50,150,100)
+oblako_kyb = pygame.rect.Rect(50, 50, 150, 100)
 kote_kyb = pygame.rect.Rect(x, y, 169, 128)
 zontik_kyb = pygame.rect.Rect(0, 0, 102, 102)
 vedro_kyb = pygame.rect.Rect(kote_kyb.x - 15, kote_kyb.y - 30, 72, 72)
+caplya_kyb = pygame.rect.Rect(oblako_kyb.centerx, oblako_kyb.centery + 20, 20, 40)
+zatop_kyb=pygame.rect.Rect(1,570,800,30)
 move_subjects()
--- view.py
+++ view.py
@@ -5,6 +5,8 @@ kote = pygame.image.load('real kartinki/cat1.png')
 zontik = pygame.image.load('real kartinki/umbrella.png')
 vedro = pygame.image.load('real kartinki/bucket.png')
 oblako=pygame.image.load('real kartinki/cloud.png')
+caplya=pygame.image.load('real kartinki/water_drop.png')
+zatoplenie=pygame.image.load('real kartinki/water.png')
 
 zontik = pygame.transform.scale(zontik, [100, 100])
 
@@ -12,16 +14,16 @@ vedro = pygame.transform.scale(vedro, [70, 70])
 
 oblako=pygame.transform.scale(oblako,model.oblako_kyb.size)
 
+caplya=pygame.transform.scale(caplya,model.caplya_kyb.size)
+
+zatoplenie=pygame.transform.scale(zatoplenie,model.zatop_kyb.size)
+
 
 
 def ion():
     global kote
     screen.fill([0,0,0])
-    if model.show_rects == True:
-        pygame.draw.rect(screen,[0,200,0],model.kote_kyb,4)
-        zonkyb = pygame.draw.rect(screen, [200,0, 0], model.zontik_kyb, 4)
-        vedrokyb = pygame.draw.rect(screen, [100,50,0], model.vedro_kyb, 4)
-        oblakokyb=pygame.draw.rect(screen,[244,233,255],model.oblako_kyb,4)
+
     if model.katflip==1:
         kote2i0=pygame.transform.flip(kote,True,False)
         zontik2i0=pygame.transform.flip(zontik,True,False)
@@ -34,6 +36,16 @@ def ion():
         screen.blit(kote,model.kote_kyb)
         screen.blit(zontik,model.zontik_kyb)
         screen.blit(vedro,model.vedro_kyb)
+    screen.blit(zatoplenie,model.zatop_kyb)
+    screen.blit(caplya,model.caplya_kyb)
     screen.blit(oblako,model.oblako_kyb)
+
+    if model.show_rects == True:
+        pygame.draw.rect(screen,[0,200,0],model.kote_kyb,4)
+        zonkyb = pygame.draw.rect(screen, [200,0, 0], model.zontik_kyb, 4)
+        vedrokyb = pygame.draw.rect(screen, [100,50,0], model.vedro_kyb, 4)
+        oblakokyb=pygame.draw.rect(screen,[244,233,255],model.oblako_kyb,4)
+        caplyakyb=pygame.draw.rect(screen,[10,10,255],model.caplya_kyb,4)
+        zatopleniekyb=pygame.draw.rect(screen,[50,10,200],model.zatop_kyb,4)
 
     pygame.display.flip()
